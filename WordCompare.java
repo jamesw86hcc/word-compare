9b9cfaf4d8cd9d925b59701faa6f61a0c704a10f
James Williams <marysa.williams@howardcc.edu>
2023-02-10 23:21:11 -0500

    First and only commit

---
+++ WordCompare.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class WordCompare {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean stop = false;
+        String word1, word2;
+
+        do {
+            System.out.println("Please enter a word:");
+            word1 = input.nextLine();
+            word1 = word1.trim();
+
+            System.out.println("Please enter a second word:");
+            word2 = input.nextLine();
+            word2 = word2.trim();
+
+            int comparison = word1.compareToIgnoreCase(word2);
+
+            if (comparison < 0)
+            {
+                System.out.println(word1 + " comes before " + word2 + ".");
+            } else if (comparison == 0)
+            {
+                System.out.println(word1 + " and "+ word2 + " are equal.");
+            } else
+            {
+                System.out.println(word2 + " comes before " + word1 + ".");
+            }
+            Boolean badResponse = false;
+            do {
+                System.out.println("Would you like to compare another set of words? (Yes/No)");
+                String response = input.nextLine();
+                if (response.equalsIgnoreCase("yes")) {
+                    stop = false;
+                    badResponse = false;
+                } else if (response.equalsIgnoreCase("no")) {
+                    stop = true;
+                    badResponse = false;
+                } else {
+                    System.out.println("Sorry, I didn't understand " + response +". Please write 'yes' or 'no'. ");
+                    badResponse = true;
+                }
+            } while (badResponse);
+        } while (!stop);
+
+        System.out.println("Goodbye.");
+    }
+}
